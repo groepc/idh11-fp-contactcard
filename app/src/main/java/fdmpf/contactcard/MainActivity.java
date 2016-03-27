6068f42d407ab2299d4999aa809f7311c9f40fc0
Frans Dubois <fdm.dubois@student.avans.nl>
2016-03-27 22:04:49 +0200

    Revert "Mijn contacten en zoek contacten toegevoegt"
    This reverts commit 0cd7ac08a88209ff4a9527ab22219e688fd50172.

--- app/src/main/java/fdmpf/contactcard/MainActivity.java
+++ app/src/main/java/fdmpf/contactcard/MainActivity.java
@@ -1,18 +1,17 @@
 package fdmpf.contactcard;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import fdmpf.contactcard.fragments.FragmentContactList;
-import fdmpf.contactcard.fragments.FragmentContactDetail;
-
 public class MainActivity extends AppCompatActivity implements
-        FragmentContactList.OnFragmentInteractionListener{
+        FragmentA.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +19,16 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
     }
 
     @Override
@@ -31,29 +40,45 @@ public class MainActivity extends AppCompatActivity implements
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
             return true;
         }
-        if (id == R.id.action_new_contact) {
-            Intent intent = new Intent(this, SearchActivity.class);
-            startActivity(intent);
-            return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onFragmentInteraction(String msg) {
         Log.i("onFragmentInteraction", msg);
+
         // Doorsturen naar andere Fragments ...
-        FragmentContactDetail info = (FragmentContactDetail)
+        FragmentB info = (FragmentB)
                 getFragmentManager().findFragmentById(R.id.fragment_b);
+
         // In Landscape, info != null
         if (info != null ) {
             info.infoText(msg);
         }
+//        // In portrait, info == null. Start activit in code
+//        else {
+//            FragmentB frag_b = new FragmentB();
+//            Bundle args = new Bundle();
+//            args.putString("var_1", "Dit is een string");
+//            frag_b.setArguments(args);
+//
+//            //
+//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            transaction.replace(R.id.fragment_container, frag_b);
+//            transaction.addToBackStack(null);
+//
+//            // Commit
+//            transaction.commit();
+//        }
     }
 }
